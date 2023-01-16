f4d76074bc1af148bbd091b7ccff2422ee59c02f
WDissney <denrom1990@yandex.ru>
2023-01-16 16:05:34 +0300

    version 1.4 add CarService interface, rework getListCar

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,13 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarService;
+import web.service.CarServiceImpl;
 
 
 @Controller
 public class CarController {
     @Autowired
-    public CarService cars;
+    public CarServiceImpl cars;
     @GetMapping(value = "/cars")
     public String getCars(Model model, @RequestParam(value = "count", defaultValue = "5") int count){
         model.addAttribute("cars", cars.getListCars(count));
